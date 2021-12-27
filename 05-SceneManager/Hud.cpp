6906a34b2e082330f29f5d798afd5063542fd287
Ngọc Quý Đỗ <quydn720@gmail.com>
2021-12-27 15:52:14 +0700

    not scale but overdraw hud

--- 05-SceneManager/Hud.cpp
+++ 05-SceneManager/Hud.cpp
@@ -1,4 +1,10 @@
-#include "Hud.h"
+﻿#include "Hud.h"
+#include "Mario.h"
+#include "Camera.h"
+
+#define MAX_STACK_SPEED 6
+#define BASE_ARROW_POSITION_X 80
+#define BASE_ARROW_POSITION_Y 178
 
 Hud* Hud::_instance = nullptr;
 
@@ -9,10 +15,85 @@ Hud* Hud::GetInstance()
 	return _instance;
 }
 
+Hud::Hud()
+{
+	NumSpeed = 0;
+	bbox = CTextures::GetInstance()->Get(BBOX_BLACK);
+	auto sprites = CSprites::GetInstance();
+	speed = sprites->Get(ID_ANI_ARROW);
+}
+
+void Hud::Update(DWORD dt)
+{
+	float vx, vy;
+	CMario::GetInstance()->GetSpeed(vx, vy);
+
+	if (0.1f < abs(vx) && abs(vx) <= 0.115f)
+	{
+		NumSpeed = 1;
+	}
+	else
+	{
+		if (0.115f < abs(vx) && abs(vx) <= 0.13f)
+		{
+			NumSpeed = 2;
+		}
+		else
+		{
+			if (0.13f < abs(vx) && abs(vx) <= 0.15f)
+			{
+				NumSpeed = 3;
+			}
+			else
+			{
+				if (0.15f < abs(vx) && abs(vx) <= 0.17f)
+				{
+					NumSpeed = 4;
+				}
+				else
+				{
+					if (0.17f < abs(vx) && abs(vx) <= 0.19f)
+					{
+						NumSpeed = 5;
+					}
+					else
+					{
+						if (abs(vx) >= MARIO_RUNNING_SPEED)
+						{
+							NumSpeed = 6;
+						}
+					}
+				}
+			}
+		}
+	}
+	DebugOut(L"%0.2d\n", NumSpeed);
+}
+
 void Hud::Render()
 {
-	LPTEXTURE bbox = CTextures::GetInstance()->Get(35);
 	CGame* game = CGame::GetInstance();
 	RECT* r = NULL;
-	game->Draw(float(game->GetBackBufferWidth()) / 2, game->GetBackBufferHeight() - 20, bbox, r, 1.0f, game->GetBackBufferWidth(), 40);
+	CSprites* sprites = CSprites::GetInstance();
+	
+	float a = game->GetBackBufferWidth() / 2;
+	float b = game->GetBackBufferHeight() - 20;
+
+	game->Draw(a, b, bbox, r, 1.0f, game->GetBackBufferWidth(), 40);
+	
+	sprites->Get(ID_HUD_BG)->DrawFixPos(152, 183);
+	
+	if (NumSpeed > 0)
+	{
+		for (int i = 1; i <= NumSpeed; i++)
+		{
+			if (i == MAX_STACK_SPEED)
+			{
+				sprites->Get(ID_HUD_ANI_P)->DrawFixPos(float(BASE_ARROW_POSITION_X + ((6) * 8) + 11), BASE_ARROW_POSITION_Y); // fix pos
+				sprites->Get(ID_HUD_ANI_ARROW)->DrawFixPos(float(BASE_ARROW_POSITION_X + (i * 8)), BASE_ARROW_POSITION_Y);
+			}
+			else
+				sprites->Get(ID_HUD_ANI_ARROW)->DrawFixPos(float(BASE_ARROW_POSITION_X + (i * 8)), BASE_ARROW_POSITION_Y);
+		}
+	}
 }
--- 05-SceneManager/Hud.h
+++ 05-SceneManager/Hud.h
@@ -1,17 +1,28 @@
 #pragma once
 #include "Textures.h"
 #include "Game.h"
+#include "Sprite.h"
 
 #define HUD_SIZE 40
 #define ID_TEX_BBOX -100		// special texture to draw object bounding box
+#define ID_ANI_ARROW	1915
+#define ID_HUD_ANI_ARROW	1911
+#define ID_HUD_ANI_P 1912
+#define ID_HUD_BG	1910
+#define BBOX_BLACK -101
 
 class Hud
 {
 private:
 	static Hud* _instance;
 	float cam_y;
+	int NumSpeed;
+	LPTEXTURE bbox;
+	LPSPRITE speed;
 public:
 	static Hud* GetInstance();
+	Hud();
+	void Update(DWORD dt);
 	void Render();
 };
 

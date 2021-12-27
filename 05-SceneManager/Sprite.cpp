d703a1504965c42aca51d0fe0e2cc8ff33c6c485
Ngọc Quý Đỗ <quydn720@gmail.com>
2021-12-27 15:53:45 +0700

    function to draw at fixed position

--- 05-SceneManager/Sprite.cpp
+++ 05-SceneManager/Sprite.cpp
@@ -51,3 +51,23 @@ void CSprite::Draw(float x, float y)
 	g->GetSpriteHandler()->DrawSpritesImmediate(&sprite, 1, 0, 0);
 }
 
+void CSprite::DrawFixPos(float x, float y)
+{
+	CGame* g = CGame::GetInstance();
+	float cx, cy;
+	g->GetCamPos(cx, cy);
+
+	cx = (FLOAT)floor(cx);
+	cy = (FLOAT)floor(cy);
+
+	D3DXMATRIX matTranslation;
+
+	x = (FLOAT)floor(x);
+	y = (FLOAT)floor(y);
+
+	D3DXMatrixTranslation(&matTranslation, x, g->GetBackBufferHeight() - y, 0.1f);
+
+	this->sprite.matWorld = (this->matScaling * matTranslation);
+
+	g->GetSpriteHandler()->DrawSpritesImmediate(&sprite, 1, 0, 0);
+}

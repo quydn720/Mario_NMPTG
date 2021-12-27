9260b802c62f6896ca80ff9dc5246caa4d1b7c60
Ngọc Quý Đỗ <quydn720@gmail.com>
2021-12-27 15:53:51 +0700

    .

--- 05-SceneManager/Sprite.h
+++ 05-SceneManager/Sprite.h
@@ -19,6 +19,7 @@ public:
 	CSprite(int id, int left, int top, int right, int bottom, LPTEXTURE tex);
 
 	void Draw(float x, float y);
+	void DrawFixPos(float x, float y);
 };
 
 typedef CSprite* LPSPRITE;

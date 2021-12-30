e3770f553d3970f94ff2211e55a86b1d8ba0a9b0
Ngọc Quý Đỗ <quydn720@gmail.com>
2021-12-30 18:38:41 +0700

    tail collisions koopas & txt files

--- 05-SceneManager/Tail.cpp
+++ 05-SceneManager/Tail.cpp
@@ -8,6 +8,7 @@
 #include "BreakableBrick.h"
 #include "ButtonP.h"
 #include "AssetIDs.h"
+#include "Koopas.h"
 
 void CTail::Render() {
 	RenderBoundingBox();
@@ -15,16 +16,65 @@ void CTail::Render() {
 
 void CTail::GetBoundingBox(float& left, float& top, float& right, float& bottom)
 {
-	left = x - TAIL_LENGTH / 2;
-	top = y - TAIL_HEIGHT / 2 + 4;
-	right = left + TAIL_LENGTH;
-	bottom = top + TAIL_HEIGHT;
+	left = x - MARIO_TAIL_WIDTH / 2;
+	top = y - MARIO_TAIL_HEIGHT / 2 + 4;
+	right = left + MARIO_TAIL_WIDTH;
+	bottom = top + MARIO_TAIL_HEIGHT;
 }
 
 void CTail::Update(DWORD dt, vector<LPGAMEOBJECT>* coObjects)
 {
+	if (CMario::GetInstance()->level != MARIO_LEVEL_TAIL)
+	{
+		this->isDeleted = true;
+		return;
+	}
+	else
+	{
+		nx = CMario::GetInstance()->nx;
+		if (CMario::GetInstance()->nx == 1)
+		{
+			float temp_x = CMario::GetInstance()->x - 7;
+			if (CMario::GetInstance()->IsAttack == false)
+			{
+				this->x = temp_x;
+				float temp_y = CMario::GetInstance()->y;
+				this->y = temp_y - MARIO_TAIL_HEIGHT + 7;
+			}
+			else
+			{
+
+				this->SetPosition(CMario::GetInstance()->x + (MARIO_BIG_BBOX_WIDTH / 2) + (TAIL_BBOX_WIDTH / 2) + 2, CMario::GetInstance()->y + TAIL_BBOX_HEIGHT);
+
+				if (GetTickCount64() - CMario::GetInstance()->AttackTime >= RACOON_ATTACK_TIME)
+				{
+					CMario::GetInstance()->IsAttack = false;
+					IsActive = false;
+				}
+			}
+		}
+		else if (CMario::GetInstance()->nx == -1)
+		{
+			if (CMario::GetInstance()->IsAttack == false)
+			{
+				this->x = CMario::GetInstance()->x + MARIO_BIG_BBOX_WIDTH - 5;
+				float temp_y = CMario::GetInstance()->y;
+				this->y = temp_y - MARIO_TAIL_HEIGHT + 7;
+			}
+			else
+			{
+				this->SetPosition((CMario::GetInstance()->x - MARIO_BIG_BBOX_WIDTH / 2 - TAIL_BBOX_WIDTH / 2) - 2, CMario::GetInstance()->y + TAIL_BBOX_HEIGHT);
+				if (GetTickCount64() - CMario::GetInstance()->AttackTime >= RACOON_ATTACK_TIME)
+				{
+					CMario::GetInstance()->IsAttack = false;
+					IsActive = false;
+				}
+			}
+		}
+	}
+
 	for (int i = 0; i < coObjects->size(); i++) {
-		if (CCollision::GetInstance()->CheckAABB(this, coObjects->at(i)))
+		if (CCollision::GetInstance()->CheckAABB(this, coObjects->at(i)) && CMario::GetInstance()->IsAttack == true)
 		{
 			if (dynamic_cast<CGoomba*>(coObjects->at(i)))
 			{
@@ -36,8 +86,10 @@ void CTail::Update(DWORD dt, vector<LPGAMEOBJECT>* coObjects)
 				qb->SpawnItem(CMario::GetInstance()->getNx(), MARIO_LEVEL_TAIL);
 			}
 			else if (dynamic_cast<CKoopas*>(coObjects->at(i))) {
-				CKoopas* qb = dynamic_cast<CKoopas*>(coObjects->at(i));
-				qb->SetState(KOOPAS_STATE_SHELL);
+				CKoopas* koopas = dynamic_cast<CKoopas*>(coObjects->at(i));
+				koopas->SetState(KOOPAS_STATE_SHELL_2);
+				koopas->vy = -0.4f;
+				koopas->ReviveTime = GetTickCount64();
 			}
 			else if (dynamic_cast<BreakableBrick*>(coObjects->at(i)))
 				OnCollisionWithBreakableBrick(coObjects->at(i));
@@ -46,10 +98,18 @@ void CTail::Update(DWORD dt, vector<LPGAMEOBJECT>* coObjects)
 }
 
 void CTail::OnCollisionWith(LPCOLLISIONEVENT e) {
-	if (state == TAIL_ATTACK) {
+	if (CMario::GetInstance()->IsAttack == true)
+	{
 		if (dynamic_cast<CGoomba*>(e->obj)) {
 			CGoomba* g = dynamic_cast<CGoomba*>(e->obj);
 			g->SetState(GOOMBA_STATE_DIE);
+		}
+		else if (dynamic_cast<CKoopas*>(e->obj)) {
+			CKoopas* koopas = dynamic_cast<CKoopas*>(e->obj);
+			koopas->SetState(KOOPAS_STATE_SHELL_2);
+			koopas->vy = -0.4f;
+			koopas->ReviveTime = GetTickCount64();
+			
 		}
 		else if (dynamic_cast<CQuestionBlock*>(e->obj)) {
 			CQuestionBlock* qb = dynamic_cast<CQuestionBlock*>(e->obj);

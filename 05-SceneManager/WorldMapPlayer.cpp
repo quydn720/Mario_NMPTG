8944e964e4e3f2f9aac9a77240c675b38719a152
dnq2k <18521313@gm.uit.edu.vn>
2023-02-16 13:21:52 +0700

    add world map player class

---
+++ 05-SceneManager/WorldMapPlayer.cpp
@@ -0,0 +1,89 @@
+#include "WorldMapPlayer.h"
+#include "Animations.h"
+#include "debug.h"
+#include "Collision.h"
+
+void WorldMapPlayer::Render()
+{
+	CAnimations::GetInstance()->Get(-28127 + 200)->Render(x, y);
+}
+
+void WorldMapPlayer::OnKeyDown(int KeyCode)
+{
+	
+}
+
+void WorldMapPlayer::KeyState(BYTE* state)
+{
+}
+
+void WorldMapPlayer::OnKeyUp(int KeyCode)
+{
+}
+
+void WorldMapPlayer::Update(DWORD dt, int currentY, int currentX, int& moved)
+{
+	//DebugOut(L"my: %f, \n", my);
+
+	x += vx * dt;
+	y += vy * dt;
+
+	//DebugOut(L"%f\t\n", y);// 64 - (currentX * 16));
+	
+	if (vx > 0) {
+		if (x > ((currentY - 1) * 16 + 64)) {
+			Stop();
+			moved = 0;
+		}
+	}
+	if (vx < 0) {
+		if (x < ((currentY - 1) * 16 + 64)) {
+			Stop();
+			moved = 0;
+		}
+	}
+	if (vy > 0) {
+		//if (y > 64 - currentX * 15) {
+
+		/*if (y > my - (moved * 15)) {
+			Stop();
+			moved = 0;
+		}*/
+	}
+	if (vy < 0) {
+		if (y < abs(my - (moved) * 16)) {
+			Stop();
+			moved = 0;
+			my = abs((moved) * 15);
+		}
+	}
+	
+
+	/*CCollision* collision = new CCollision();
+	collision->CheckAABB()*/
+	/*float Left, Top, Right, Bottom, left, top, right, bottom;
+	Left = x;
+	Top = y;
+	Right = Left + 14;
+	Bottom = Top + 16;
+	
+	
+	left = 112;
+	top = 64;
+	right = left + 16;
+	bottom = top + 16;
+
+	bool colide = (!(Left > right || Top > bottom || Right < left || Bottom < top));*/
+
+	/*if (x < 62) {
+		x = 62; 
+		isMoving = false; vx = 0; vy = 0;
+	}
+
+	if (x > 94) {
+		x = 94;
+		isMoving = false; vx = 0; vy = 0; 
+	}*/
+
+	//DebugOut(L"%0.2f\t%0.2f\n", vx, vy);
+}
---
+++ 05-SceneManager/WorldMapPlayer.h
@@ -0,0 +1,46 @@
+#pragma once
+#include "KeyEventHandler.h"
+
+class WorldMapPlayer : public CKeyEventHandler
+{
+private: 
+	float x, y;
+	float vx, vy;
+	bool isMoving;
+
+	float my;
+public:
+	WorldMapPlayer() {
+		x = 62; y = 64; my = 0;
+		vx = 0.0f; vy = 0.0f;
+		isMoving = false;
+	}
+	WorldMapPlayer(float x, float y) {
+		this->x = x; this->y = y;  this->my = y;
+		vx = 0.0f; vy = 0.0f;
+		isMoving = false;
+	}
+
+	float GetX() { return x; }
+	float GetY() { return y; }
+
+	bool isNotMoving() { return isMoving == false; }
+
+	void GoUp() { vy = -0.1f; vx = 0; isMoving = true; }
+	void GoDown() { vy = 0.1f; vx = 0; isMoving = true; }
+	void GoLeft() {
+		vx = -0.1f; vy = 0; isMoving = true; 
+	}
+	void GoRight() {
+		vx = 0.1f; vy = 0; isMoving = true; 
+	}
+	void Stop() { vx = 0; vy = 0; isMoving = false;}
+
+	void Render();
+	void Update(DWORD dt, int currentY, int currentX, int& moved);
+
+	void OnKeyDown(int KeyCode);
+	void KeyState(BYTE* state);
+	void OnKeyUp(int KeyCode);
+};
+

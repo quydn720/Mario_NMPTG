9da511feecc09dd350d10aa610eb9a767e631388
dnq2k <18521313@gm.uit.edu.vn>
2023-02-16 13:26:19 +0700

    handle key on wm

--- 05-SceneManager/WorldMapKeyHandler.cpp
+++ 05-SceneManager/WorldMapKeyHandler.cpp
@@ -1,4 +1,7 @@
 #include "WorldMapKeyHandler.h"
+#include <dinput.h>
+#include "debug.h"
+#include "Game.h"
 
 void WorldMapKeyHandler::KeyState(BYTE* states)
 {
@@ -6,6 +9,32 @@ void WorldMapKeyHandler::KeyState(BYTE* states)
 
 void WorldMapKeyHandler::OnKeyDown(int KeyCode)
 {
+	WorldMapScene *scene = (WorldMapScene*)scence;
+
+	if (player->isNotMoving()) {
+		if (KeyCode == DIK_S) {
+			CGame::GetInstance()->InitiateSwitchScene(1);
+		}
+
+		switch (KeyCode) {
+			case DIK_DOWN:
+				scene->GoDown();
+				break;
+			case DIK_UP:
+				scene->GoUp();
+				break;
+			case DIK_LEFT: {
+				scene->GoLeft();
+				break;
+			}
+			case DIK_RIGHT: {
+				scene->GoRight();
+				break;
+			}
+			default:
+				break;
+			}
+	}
 }
 
 void WorldMapKeyHandler::OnKeyUp(int KeyCode)
--- 05-SceneManager/WorldMapKeyHandler.h
+++ 05-SceneManager/WorldMapKeyHandler.h
@@ -1,12 +1,18 @@
 #pragma once
 #include "Scene.h"
+#include "WorldMapPlayer.h"
 
 class WorldMapKeyHandler : public CSceneKeyHandler
 {
+
+private: 
+	WorldMapPlayer* player;
 public:
 	virtual void KeyState(BYTE* states);
 	virtual void OnKeyDown(int KeyCode);
 	virtual void OnKeyUp(int KeyCode);
-	WorldMapKeyHandler(LPSCENE s) : CSceneKeyHandler(s) {};
+	WorldMapKeyHandler(LPSCENE s, WorldMapPlayer* p) : CSceneKeyHandler(s) {
+		player = p;
+	};
 };
 

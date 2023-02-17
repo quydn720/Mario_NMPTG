ce020dddcbae3da895f6aa7cb8ef06915a3c656f
quydn720 <quydn720@gmail.com>
2023-02-17 13:42:10 +0700

    extract constant, var

--- 05-SceneManager/WorldMapPlayer.cpp
+++ 05-SceneManager/WorldMapPlayer.cpp
@@ -3,9 +3,19 @@
 #include "debug.h"
 #include "Collision.h"
 
+#define TILE_SIZE 16
+#define WORLD_MAP_PLAYER_MOVING_ID -27927
+
+WorldMapPlayer::WorldMapPlayer()
+{
+	x = 64; y = 64; startX = 0; startY = 0;
+	vx = 0.0f; vy = 0.0f;
+	isMoving = false;
+}
+
 void WorldMapPlayer::Render()
 {
-	CAnimations::GetInstance()->Get(-28127 + 200)->Render(x, y);
+	CAnimations::GetInstance()->Get(WORLD_MAP_PLAYER_MOVING_ID)->Render(x, y);
 }
 
 void WorldMapPlayer::OnKeyDown(int KeyCode)
@@ -33,26 +43,28 @@ void WorldMapPlayer::Update(DWORD dt, int currentY, int currentX, int& moved)
 	x += vx * dt;
 	y += vy * dt;
 
+	float distance = moved * TILE_SIZE;
+
 	if (vx > 0) {
-		if (x > ((moved) * 16 + startX)) {
+		if (x > (distance + startX)) {
 			Stop();
 			moved = 0;
 		}
 	}
 	if (vx < 0) {
-		if (x < startX - ((moved) * 16)) {
+		if (x < startX - (distance)) {
 			Stop();
 			moved = 0;
 		}
 	}
 	if (vy > 0) {
-		if (y > startY + ((moved) * 16)) {
+		if (y > startY + (distance)) {
 			Stop();
 			moved = 0;
 		}
 	}
 	if (vy < 0) {
-		if (y < startY - ((moved) * 16)) {
+		if (y < startY - distance) {
 			Stop();
 			moved = 0;
 		}

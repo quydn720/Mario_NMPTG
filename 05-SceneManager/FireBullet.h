815846eed4f146472d0aafd0fd51c0c14ad2b597
Ngọc Quý Đỗ <18521313@gm.uit.edu.vn>
2023-12-12 15:16:37 +0700

    the bullet will be aim at Mario at the initial state

--- 05-SceneManager/FireBullet.h
+++ 05-SceneManager/FireBullet.h
@@ -1,5 +1,8 @@
 #pragma once
 #include "GameObject.h"
+#include "Mario.h"
+#define BULLET_SPEED -0.001f
+
 class FireBullet : public CGameObject
 {
 private:
@@ -7,7 +10,11 @@ private:
 public:
 	FireBullet(float x, float y, float vx, float vy) {
 		this->x = x; this->y = y;
-		this->vy = vy; this->vx = vx;
+
+		float mx, my;
+		CMario::GetInstance()->GetPosition(mx, my);
+		this->vx = (x - mx) * BULLET_SPEED;
+		this->vy = (y - my) * BULLET_SPEED;
 	};
 	void Update(DWORD dt, vector<LPGAMEOBJECT>* coObjects);
 	void Render();
